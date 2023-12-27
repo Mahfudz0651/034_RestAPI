c99efe39e44d9acd5c6671a52a973b92b31b152c
TC1034 <mahfudzsiddiq0@gmail.com>
2023-12-27 22:36:26 +0700

    Add FloatingActionButton HomeScreen

--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -68,8 +69,16 @@ fun HomeScreen(
                 scrollBehavior = scrollBehavior,
                 )
         },
-    )
-    {
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = navigateToItemEntry,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(18.dp)
+                ) {
+
+            }
+        }
+    ) {
     }
 }
 
