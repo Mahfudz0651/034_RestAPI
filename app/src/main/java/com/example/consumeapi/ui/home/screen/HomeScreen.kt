346e4276a047fa0ee4f62fd3fde61a00cf1c4287
TC1034 <mahfudzsiddiq0@gmail.com>
2023-12-27 22:02:34 +0700

    Add onDetailClick And OndeleteClick

--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -82,14 +82,21 @@ fun OnLoading(modifier: Modifier = Modifier){
 @Composable
 fun KontakLayout(
     kontak : List<Kontak>,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onDetailClick: (Kontak) -> Unit,
+    onDeleteClick: (Kontak) -> Unit = {}
 ){
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)){
         items(kontak){kontak ->
-            KontakCard(kontak = kontak, modifier = Modifier.fillMaxWidth())
+            KontakCard(
+                kontak = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+
+            )
         }
     }
 }
