29e7222fd2bf7eb18cfb88d54412f38cf67e6cea
TC1034 <mahfudzsiddiq0@gmail.com>
2023-12-27 22:09:34 +0700

    Add onDetailClick And OndeleteClick in KontakLayout

--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.consumeapi.ui.home.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -95,7 +96,10 @@ fun KontakLayout(
                 kontak = kontak,
                 modifier = Modifier
                     .fillMaxWidth()
-
+                    .clickable { onDetailClick(Kontak)},
+                onDeleteClick = {
+                    onDeleteClick(Kontak)
+                }
             )
         }
     }
