55c39daaf72cb40a129eedd7c95b92068816e3ca
TC1034 <mahfudzsiddiq0@gmail.com>
2023-12-28 10:17:49 +0700

    Add Initializer in PenyediaViewModelewmodel

--- app/src/main/java/com/example/consumeapi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/consumeapi/ui/PenyediaViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.consumeapi.ui
 
+import android.os.Build
 import android.text.Spannable.Factory
+import androidx.annotation.RequiresExtension
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
@@ -8,12 +10,17 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.consumeapi.KontakAplikasi
 import com.example.consumeapi.ui.home.viewmodel.HomeViewModel
+import com.example.consumeapi.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     val Factory = viewModelFactory {
 
         initializer {
             HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
         }
     }
 }
